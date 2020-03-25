54d752f7285fe284924dec6795c6245e2f50bb94
Jamie Barton <jamie@notrab.dev>
2020-03-25 07:23:41 +0000

    update to latest versions

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,3 @@
-const tailwindcss = require('tailwindcss')
-
 module.exports = {
-  plugins: [
-    require('postcss-easy-import'),
-    tailwindcss('./tailwind.js'),
-    require('autoprefixer')
-  ]
-}
+  plugins: ["tailwindcss", "postcss-preset-env"]
+};
